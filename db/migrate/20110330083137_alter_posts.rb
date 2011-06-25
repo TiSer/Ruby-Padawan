058dc4eacafed0029ca388babe1d4b3549311d0b
TiSer <toss35@ua.fm>
2011-06-25 14:22:03 +0300

    User migration editing-2

--- db/migrate/20110330083137_alter_posts.rb
+++ db/migrate/20110330083137_alter_posts.rb
@@ -1,7 +1,7 @@
 class AlterPosts < ActiveRecord::Migration
   def self.up
 #	add_column :posts, :updated_at, :datetime
-	change_column :posts, :author, :integer, :default => 0, :null => false
+	change_column :posts, :author, :string, :default => 0, :null => false
 	rename_column :posts, :author, :author_id
 	add_index :posts, :author_id
   end
@@ -11,7 +11,7 @@ class AlterPosts < ActiveRecord::Migration
 	remove_index :posts, :author_id
 	rename_column :posts, :author_id, :author
 	change_column :posts, :author, :string, :default => 0, :null => false
-false
+
 	#raise ActiveRecord::IrreversibleMigration
 	# (if converting ingeres back to string should not be allowed)
   end
